cd57bce720baa5e376b5a5a00d55cb32c295771a
thdrudtp15 <15rudtp@naver.com>
2024-10-05 15:24:41 +0900

    Feat : 이미지 핸들러

--- components/lib/Editor.tsx
+++ components/lib/Editor.tsx
@@ -3,26 +3,48 @@
 import styles from './Editor.module.scss'
 import './ReactQuill.css'
 import ReactQuill from 'react-quill'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Input from '../common/Input'
 
 const modules = {
-  toolbar: [
-    ['blockquote', 'code-block'],
-    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-    ['link'],
-    [{ header: 1 }, { header: 2 }], // custom button values
-    [{ list: 'ordered' }, { list: 'bullet' }],
-    [{ direction: 'rtl' }], // text direction
-    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-    [{ font: [] }],
-    [{ align: [] }],
-    ['clean'], // remove formatting button
-  ],
-  // 순서를 잘 구성해서 작성할 것
+  toolbar: {
+    container: [
+      ['blockquote', 'code-block'],
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+      ['link'],
+      [{ header: 1 }, { header: 2 }], // custom button values
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ align: [] }],
+      ['image'],
+      ['clean'], // remove formatting button
+    ],
+    handlers: {
+      image: function () {
+        const input = document.createElement('input')
+        input.setAttribute('type', 'file')
+        input.setAttribute('accept', 'image/*')
+        input.addEventListener('change', (event) => {
+          const target = event.target as HTMLInputElement
+          if (target.files && target.files[0]) {
+            console.log(target.files[0])
+            let src = URL.createObjectURL(target.files[0])
+          }
+        })
+        input.click()
+
+        // 1. 클릭을 함
+        // 2. 이미지를 마운트 함.
+        // 3. 이미지 마운트 한 걸 S3에 업로드 함
+        // 4. 주소를 받아와서
+        // 5. 현재 커서 위치에 <img sr~/> 이렇게 넣음
+      },
+    },
+  },
 }
+
 const formats = [
   'font',
   'code-block',
@@ -46,6 +68,7 @@ export default function Editor() {
   const [title, setTitle] = useState<string>('')
   const [content, setContent] = useState<string>('')
   const [publicOption, setPublicOption] = useState<boolean>(false)
+  const quillRef = useRef(null)
 
   const write = async () => {
     const _res = await fetch('/api/post', {
@@ -80,6 +103,7 @@ export default function Editor() {
           placeholder="글 작성해주세요"
           className={styles.editor}
           formats={formats}
+          ref={quillRef}
         />
       </div>
       <div className={`${styles.editor_section} ${styles.section_prv}`}>
